fa6b25b22bfee9a7aefad416f2d5ed7d188a7926
Nolita Rego <48682340+nolita26@users.noreply.github.com>
2021-05-03 11:09:43 +0530

    Update RecommendationsPage.dart

--- stress_detector/lib/Pages/RecommendationsPage.dart
+++ stress_detector/lib/Pages/RecommendationsPage.dart
@@ -1,16 +1,12 @@
 import 'dart:async';
-import 'dart:convert';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_twitter_login/flutter_twitter_login.dart';
-import 'package:http/http.dart' as http;
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:stress_detector/Essentials/ThemeColor.dart';
 import 'package:stress_detector/LoginPage.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class PlacesSearchMapSample extends StatefulWidget {
-
   @override
   State<PlacesSearchMapSample> createState() {
     return _PlacesSearchMapSample();
@@ -20,42 +16,18 @@ class PlacesSearchMapSample extends StatefulWidget {
 class _PlacesSearchMapSample extends State<PlacesSearchMapSample> {
 
   static const String _API_KEY = 'AIzaSyAIFCtkfQICnpKwRiHWbryLqQ-Sb2RVOIA';
-
-  static double latitude = 19.083140;
-  static double longitude = 72.851160;
-  static const String baseUrl =
-      "https://maps.googleapis.com/maps/api/place/nearbysearch/json";
-
-  _launchMap() async {
-    final String googleMapsUrl = "comgooglemaps://?center=$latitude,$longitude";
-    if (await canLaunch(googleMapsUrl)) {
-    await launch(googleMapsUrl);
-    } else {
-    throw 'Could not launch $googleMapsUrl';
-    }
-  }
-
-  List<Marker> markers = <Marker>[];
-  Error error;
-  List<Result> places;
-  bool searching = true;
-  final String keyword = 'doctor';
-
-  Completer<GoogleMapController> _controller = Completer();
-
-  static final CameraPosition _myLocation = CameraPosition(
-      target: LatLng(latitude, longitude),
-      zoom: 20,
-      bearing: 15.0,
-      tilt: 75.0
-  );
-
   FirebaseAuth _auth = FirebaseAuth.instance;
   final TwitterLogin twitterLogin = new TwitterLogin(
       consumerKey: '9b8smdO0UloxZojzQ3Eh4zR7e',
       consumerSecret: 'MhmYPAiSSeoThxzvGtpSfwEQKuklKbDkQeen9q2Wrsb9bJjhJL'
   );
 
+  Completer<GoogleMapController> _controller = Completer();
+  @override
+  void initState() {
+    super.initState();
+  }
+  double zoomVal=5.0;
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -85,214 +57,304 @@ class _PlacesSearchMapSample extends State<PlacesSearchMapSample> {
           )
         ],
       ),
-      body: GoogleMap(
+      body: Stack(
+        children: <Widget>[
+          _buildGoogleMap(context),
+          _zoomminusfunction(),
+          _zoomplusfunction(),
+          _buildContainer(),
+        ],
+      ),
+    );
+  }
+
+  Widget _zoomminusfunction() {
+    return Align(
+      alignment: Alignment.topLeft,
+      child: IconButton(
+          icon: Icon(Icons.zoom_out,color: kThemeColor, size: 40),
+          onPressed: () {
+            zoomVal--;
+            _minus( zoomVal);
+          }),
+    );
+  }
+  Widget _zoomplusfunction() {
+    return Align(
+      alignment: Alignment.topRight,
+      child: IconButton(
+          icon: Icon(Icons.zoom_in,color: kThemeColor, size: 40,),
+          onPressed: () {
+            zoomVal++;
+            _plus(zoomVal);
+          }),
+    );
+  }
+
+  Future<void> _minus(double zoomVal) async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng(19.081822, 72.851561), zoom: zoomVal)));
+  }
+  Future<void> _plus(double zoomVal) async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng(19.081822, 72.851561), zoom: zoomVal)));
+  }
+
+  Widget _buildContainer() {
+    return Align(
+      alignment: Alignment.bottomLeft,
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 20.0),
+        height: 150.0,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+            SizedBox(width: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes(
+                  "https://lh5.googleusercontent.com/p/AF1QipMPZnBuaULhDRG08-GBqGZTnkkJ510tcqn2RgAr=w408-h306-k-no",
+                  19.080622447835268, 72.84220135423166,"R K Hospital"),
+            ),
+            SizedBox(width: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes(
+                  "https://lh5.googleusercontent.com/p/AF1QipPlSd5diiq9FgWfCLr9Kra-xymgI5oQDIqB3zLe=w408-h544-k-no",
+                  19.07741985809697, 72.85817353888743,"Niron Hospital"),
+            ),
+            SizedBox(width: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes(
+                  "https://lh5.googleusercontent.com/p/AF1QipNUBOlD-QPn2ZDin5lZNkbqvGMXWpUnvzUZo-ZO=w425-h240-k-no",
+                  19.08089202913187, 72.85463538320347,"Aseem Hospital"),
+            ),
+            SizedBox(width: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes(
+                  "https://lh5.googleusercontent.com/p/AF1QipP1_8aNIgxho7uwsbmFdzCIi7Hk2OEzwev2QVpd=w426-h240-k-no",
+                  19.078968673512403, 72.84505134689485,"V N Desai Hospital"),
+            ),
+            SizedBox(width: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes(
+                  "https://lh5.googleusercontent.com/p/AF1QipMIoEVQtkDFnCJfjMZ7uLVRl0go-03rXnwdLI9-=w408-h725-k-no",
+                  19.073194657932994, 72.84455627555363,"Sanjeevani Hospital"),
+            ),
+            SizedBox(width: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes(
+                  "https://lh5.googleusercontent.com/p/AF1QipNDQhhtmbiXM31U1wHBYE7W9X9zBqEXb89-RmK0=w408-h306-k-no",
+                  19.079106758007857, 72.86631646342134,"Sahyog Wellcare Hospital"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _boxes(String _image, double lat,double long,String hospName) {
+    return  GestureDetector(
+      onTap: () {
+        _gotoLocation(lat,long);
+      },
+      child:Container(
+        child: new FittedBox(
+          child: Material(
+            color: Colors.white,
+            elevation: 14.0,
+            borderRadius: BorderRadius.circular(24.0),
+            shadowColor: Color(0x802196F3),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  width: 180,
+                  height: 200,
+                  child: ClipRRect(
+                    borderRadius: new BorderRadius.circular(24.0),
+                    child: Image(
+                      fit: BoxFit.fill,
+                      image: NetworkImage(_image),
+                    ),
+                  ),),
+                Container(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: myDetailsContainer1(hospName),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget myDetailsContainer1(String hospName) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: Container(
+              child: Text(hospName,
+                style: TextStyle(
+                    color: kThemeColor,
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold),
+              )),
+        ),
+        SizedBox(height:5.0),
+        Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Container(
+                    child: Text(
+                      "4.1",
+                      style: TextStyle(
+                        color: Colors.black54,
+                        fontSize: 18.0,
+                      ),
+                    )),
+                Container(
+                  child: Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                    size: 15.0,
+                  ),
+                ),
+                Container(
+                  child: Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                    size: 15.0,
+                  ),
+                ),
+                Container(
+                  child: Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                    size: 15.0,
+                  ),
+                ),
+                Container(
+                  child: Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                    size: 15.0,
+                  ),
+                ),
+                Container(
+                  child: Icon(
+                    Icons.star_half,
+                    color: Colors.amber,
+                    size: 15.0,
+                  ),
+                ),
+                Container(
+                    child: Text(
+                      "(1046)",
+                      style: TextStyle(
+                        color: Colors.black54,
+                        fontSize: 18.0,
+                      ),
+                    )),
+              ],
+            )),
+        SizedBox(height:5.0),
+        Container(
+            child: Text(
+              "Indian \u00B7 \u0024\u0024 \u00B7 1.9 mi",
+              style: TextStyle(
+                color: Colors.black54,
+                fontSize: 18.0,
+              ),
+            )),
+        SizedBox(height:5.0),
+        Container(
+            child: Text(
+              "Closed \u00B7 Opens 9:00 Mon",
+              style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold),
+            )),
+      ],
+    );
+  }
+
+  Widget _buildGoogleMap(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: GoogleMap(
         mapType: MapType.normal,
-        initialCameraPosition: _myLocation,
+        initialCameraPosition:  CameraPosition(target: LatLng(19.081822, 72.851561), zoom: 12),
         onMapCreated: (GoogleMapController controller) {
-          _setStyle(controller);
           _controller.complete(controller);
         },
-        markers: Set<Marker>.of(markers),
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          // searchNearby(latitude, longitude);
-          _launchMap();
+        markers: {
+          vnMarker,sahyogMarker,rkMarker,nironMarker,aseemMarker
         },
-        label: Text('Places Nearby'),
-        icon: Icon(Icons.place),
       ),
     );
   }
 
-  void _setStyle(GoogleMapController controller) async {
-    String value = await DefaultAssetBundle.of(context).loadString('assets/maps_style.json');
-    controller.setMapStyle(value);
-  }
-
-  void searchNearby(double latitude, double longitude) async {
-    setState(() {
-      markers.clear();
-    });
-    String url =
-        '$baseUrl?key=$_API_KEY&location=$latitude,$longitude&radius=800&keyword=doctor';
-    print(url);
-    final response = await http.get(url);
-
-    if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-      _handleResponse(data);
-    } else {
-      throw Exception('An error occurred getting places nearby');
-    }
-
-    // make sure to hide searching
-    setState(() {
-      searching = false;
-    });
-  }
-
-  void _handleResponse(data){
-    // bad api key or otherwise
-    if (data['status'] == "REQUEST_DENIED") {
-      setState(() {
-        error = Error.fromJson(data);
-      });
-      // success
-    } else if (data['status'] == "OK") {
-      setState(() {
-        places = PlaceResponse.parseResults(data['results']);
-        for (int i = 0; i < places.length; i++) {
-          markers.add(
-            Marker(
-              markerId: MarkerId(places[i].placeId),
-              position: LatLng(places[i].geometry.location.lat,
-                  places[i].geometry.location.long),
-              infoWindow: InfoWindow(
-                  title: places[i].name, snippet: places[i].vicinity),
-              onTap: () {},
-            ),
-          );
-        }
-      });
-    } else {
-      print(data);
-    }
+  Future<void> _gotoLocation(double lat,double long) async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng(lat, long), zoom: 15,tilt: 50.0,
+      bearing: 45.0,)));
   }
 }
 
-class Error {
-  final String errorMessage;
-  final List<dynamic> htmlAttributions;
-  final List<dynamic> results;
-  final String status;
+Marker rkMarker = Marker(
+  markerId: MarkerId('rk'),
+  position: LatLng(19.0803, 72.8422),
+  infoWindow: InfoWindow(title: 'R K Hospital'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+    BitmapDescriptor.hueViolet,
+  ),
+);
 
-  String getErrorMessage() {
-    return errorMessage;
-  }
+Marker nironMarker = Marker(
+  markerId: MarkerId('niron'),
+  position: LatLng(19.0772, 72.8582),
+  infoWindow: InfoWindow(title: 'Niron Hospital'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+    BitmapDescriptor.hueViolet,
+  ),
+);
+Marker aseemMarker = Marker(
+  markerId: MarkerId('aseem'),
+  position: LatLng(19.08089202913187, 72.85463538320347),
+  infoWindow: InfoWindow(title: 'Aseem Hospital'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+    BitmapDescriptor.hueViolet,
+  ),
+);
 
-  Error({this.errorMessage, this.htmlAttributions, this.results, this.status});
+//hosp Marker
 
-  factory Error.fromJson(Map<String, dynamic> json) {
-    return Error(
-      errorMessage: json['error_message'],
-      htmlAttributions: json['html_attributions'],
-      results: json['results'],
-      status: json['status'],
-    );
-  }
-}
-
-class Geometry {
-  final Location location;
-  final ViewPort viewPort;
-  Geometry({this.location, this.viewPort});
-
-  factory Geometry.fromJson(Map<String, dynamic> json){
-    return Geometry(
-        location: Location.fromJson(json['location']),
-        viewPort: ViewPort.fromJson(json['viewport'])
-    );
-  }
-}
-
-class Location {
-  final double lat;
-  final double long;
-  Location({this.lat, this.long});
-
-  factory Location.fromJson(Map<String, dynamic> json){
-    return Location(
-        lat: json['lat'],
-        long: json['lng']
-    );
-  }
-}
-
-class Photo {
-  int height;
-  int width;
-  List<String> htmlAttributions;
-  String photoReference;
-
-  Photo({this.height, this.width, this.htmlAttributions, this.photoReference});
-
-  factory Photo.fromJson(Map<String, dynamic> json){
-    return Photo(
-        height: json['height'],
-        width: json['width'],
-        htmlAttributions: List<String>.from(json['html_attributions']),
-        photoReference: json['photo_reference']
-    );
-  }
-}
-
-class PlaceResponse {
-  final List<String> htmlAttributions;
-  final List<Result> results;
-  final String status;
-  final String nextPageToken;
-
-  PlaceResponse({this.htmlAttributions, this.results, this.status, this.nextPageToken});
-
-  PlaceResponse fromJson(Map<String, dynamic> json) {
-    return PlaceResponse(
-        htmlAttributions: List<String>.from(json['html_attributions']),
-        nextPageToken: json['next_page_token'],
-        results: parseResults(json['results']),
-        status: json['status']);
-  }
-
-  static List<Result> parseResults(List<dynamic> list){
-    return list.map((i) => Result.fromJson(i)).toList();
-  }
-}
-
-class Result {
-  final Geometry geometry;
-  final String icon;
-  final String id;
-  final String name;
-  final List<Photo> photos;
-  final String placeId;
-  final double rating;
-  final String reference;
-  final String scope;
-  final List<String> types;
-  final int userRatingsTotal;
-  final String vicinity;
-
-  Result({this.geometry, this.icon, this.id, this.name, this.placeId, this.reference,
-    this.scope, this.vicinity, this.types, this.photos,
-    this.userRatingsTotal, this.rating});
-
-  factory Result.fromJson(Map<String, dynamic> json){
-    return Result(
-      geometry: Geometry.fromJson(json['geometry']),
-      icon: json['icon'],
-      id: json['id'],
-      name: json['name'],
-      photos: json['photos'] != null ? json['photos'].map<Photo>((i) => Photo.fromJson(i)).toList(): [],
-      placeId: json['place_id'],
-      rating: json['rating'] != null ? json['rating'].toDouble(): 0.0,
-      reference: json['reference'],
-      scope: json['scope'],
-      types: List<String>.from(json['types']),
-      userRatingsTotal: json['user_ratings_total'],
-      vicinity: json['vicinity'],
-    );
-  }
-}
-
-class ViewPort {
-  final Location northEast;
-  final Location southWest;
-  ViewPort({this.northEast, this.southWest});
-
-  factory ViewPort.fromJson(Map<String, dynamic> json){
-    return ViewPort(
-        northEast: Location.fromJson(json['northeast']),
-        southWest: Location.fromJson(json['southwest'])
-    );
-  }
-}
+Marker vnMarker = Marker(
+  markerId: MarkerId('vn'),
+  position: LatLng(19.078968673512403, 72.84505134689485),
+  infoWindow: InfoWindow(title: 'V N Desai Hospital'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+    BitmapDescriptor.hueViolet,
+  ),
+);
+Marker sahyogMarker = Marker(
+  markerId: MarkerId('sahyog'),
+  position: LatLng(19.079106758007857, 72.86631646342134),
+  infoWindow: InfoWindow(title: 'Sahyog Wellcare Hospital'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+    BitmapDescriptor.hueViolet,
+  ),
+);
