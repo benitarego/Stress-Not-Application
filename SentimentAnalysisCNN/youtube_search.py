ea65add7ba4f9146b5daf64a52e4f0cbd4cda8a4
Benita Rego <42091557+benitarego@users.noreply.github.com>
2021-05-28 11:19:59 +0530

    Update youtube_search.py

--- SentimentAnalysisCNN/youtube_search.py
+++ SentimentAnalysisCNN/youtube_search.py
@@ -11,7 +11,7 @@ import unidecode
 # tab of
 #   https://cloud.google.com/console
 # Please ensure that you have enabled the YouTube Data API for your project.
-DEVELOPER_KEY = "AIzaSyB-mExYxmNgItWzt3cju7-L3U-Fulx9Mrg"
+DEVELOPER_KEY = ""
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
