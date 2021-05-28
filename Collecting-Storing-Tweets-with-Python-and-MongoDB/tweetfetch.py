8a81672e52b7b8c6a0f28032254a06e8aecd79a8
Benita Rego <42091557+benitarego@users.noreply.github.com>
2021-05-28 11:24:46 +0530

    Update tweetfetch.py

--- Collecting-Storing-Tweets-with-Python-and-MongoDB/tweetfetch.py
+++ Collecting-Storing-Tweets-with-Python-and-MongoDB/tweetfetch.py
@@ -5,8 +5,8 @@ import sys
 import time
 import random
 
-auth = tweepy.OAuthHandler("IXo5uEathVAM3NIQcsYTioDTY", "rSfXe1a3Qk1V9B3DSrbcVJZrH402FLWvjsbiFQ9BvJ0MxWMJV6")
-auth.set_access_token("1090237519634022403-zelXU9FpzGCaFlnzR1Id96THCxUo4D", "RyRb5ZHko1Uwhv8QZcafUtBUBAZdLKrtUP6Mbmh24tRKG")
+auth = tweepy.OAuthHandler("xxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+auth.set_access_token("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
 api = tweepy.API(auth)
 
 fb_url = https://{yourfirebaseappname}.firebaseio.com/tweets.json
